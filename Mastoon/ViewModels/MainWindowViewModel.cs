d9a8728d1597f13f3a4e03456aaf4ba28ed68772
TomoyaShibata <wind.of.hometown@gmail.com>
2017-09-19 13:36:20 +0000

    :+1: Add MainWindowViewModel

---
+++ Mastoon/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+
+namespace Mastoon.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public DelegateCommand AlertCommand { get; }
+        public InteractionRequest<INotification> AlertRequest { get; } = new InteractionRequest<INotification>();
+
+        private string input;
+        public string Input
+        {
+            get { return this.input; }
+            set { this.SetProperty(ref this.input, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.AlertCommand = new DelegateCommand(() =>
+            {
+                this.AlertRequest.Raise(new Notification
+                {
+                    Title = "Alert",
+                    Content = this.Input
+                });
+            });
+        }
+    }
+}
