8308e9462ece0a1de9f51d6b41dec29236f1e379
TomoyaShibata <wind.of.hometown@gmail.com>
2017-09-22 05:58:13 +0000

    :recycle: Rename method name

--- Mastoon/ViewModels/MainWindowViewModel.cs
+++ Mastoon/ViewModels/MainWindowViewModel.cs
@@ -9,7 +9,7 @@ namespace Mastoon.ViewModels
 {
     internal class MainWindowViewModel : BindableBase
     {
-        public DelegateCommand PostCommand { get; }
+        public DelegateCommand PostStatusCommand { get; }
         public InteractionRequest<INotification> PostRequest { get; } = new InteractionRequest<INotification>();
 
         private string _input;
@@ -26,31 +26,30 @@ namespace Mastoon.ViewModels
 
         public MainWindowViewModel()
         {
-            this.PostCommand = new DelegateCommand(this.PostAsync);
+            this.PostStatusCommand = new DelegateCommand(this.PostStatusAsync);
 
             this.SetupMastodonClient();
         }
 
         private async void SetupMastodonClient()
         {
-            var authClient = new AuthenticationClient("");
+            var authClient = new AuthenticationClient("m6n.onsen.tech");
             var appRegistration = await authClient.CreateApp("Mastdoon", Scope.Read | Scope.Write | Scope.Follow);
 
-            var auth = await authClient.ConnectWithPassword("",
-                "");
-
+            var auth = await authClient.ConnectWithPassword("wind.of.hometown+m6n.onsen.tech@gmail.com",
+                "Bn0Qi5FbEH7pdjTc");
             this._mastodonClient = new MastodonClient(appRegistration, auth);
-            this.HogeAsync();
+            this.StartGetPublicStreamingAsync();
         }
 
-        public async void HogeAsync()
+        public async void StartGetPublicStreamingAsync()
         {
             var streaming = this._mastodonClient.GetPublicStreaming();
             streaming.OnUpdate += (sender, e) => this.Statuses.Insert(0, e.Status);
             await streaming.Start();
         }
 
-        public async void PostAsync()
+        public async void PostStatusAsync()
         {
             await this._mastodonClient.PostStatus(
                 "テスト",
