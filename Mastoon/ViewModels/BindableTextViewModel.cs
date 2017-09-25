280b53e1c9b4da966320fd8e5f1917255c8be2ac
TomoyaShibata <wind.of.hometown@gmail.com>
2017-09-25 10:15:27 +0000

    :+1: Add classes for BindableTextBlock feature

---
+++ Mastoon/ViewModels/BindableTextViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Reactive.Bindings;
+
+namespace Mastoon.ViewModels
+{
+    public class BindableTextViewModel
+    {
+        public ReactiveProperty<string> Text { get; set; }
+    }
+
+    public class HyperlinkViewModel : BindableTextViewModel
+    {
+        public ReactiveProperty<Uri> Uri { get; set; }
+    }
+}
