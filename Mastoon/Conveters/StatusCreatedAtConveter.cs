79023de34e79514cc3cd7cab62df6b6d918eccdc
TomoyaShibata <wind.of.hometown@gmail.com>
2017-09-26 19:00:38 +0900

    :+1: Add StatusCreatedAtConveter

---
+++ Mastoon/Conveters/StatusCreatedAtConveter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mastoon.Conveters
+{
+    public class StatusCreatedAtConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var createdAt = (DateTime) value;
+            return TimeZoneInfo.ConvertTimeFromUtc(createdAt, TimeZoneInfo.Local).ToString("yyyy/MM/dd HH:mm:ss");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
