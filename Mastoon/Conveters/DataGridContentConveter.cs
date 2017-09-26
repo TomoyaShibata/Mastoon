4987564c05a3bcdb50800765ba5727a333aae97c
TomoyaShibata <wind.of.hometown@gmail.com>
2017-09-26 03:38:55 +0000

    :+1: Add content conveter to show DataGrid

---
+++ Mastoon/Conveters/DataGridContentConveter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mastoon.Conveters
+{
+    public class DataGridContentConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml((string) value);
+            return doc.DocumentNode.InnerText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
