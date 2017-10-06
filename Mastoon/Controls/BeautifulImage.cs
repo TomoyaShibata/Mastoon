c9ab6ebd82dccf62559b70f0995245766b1e2bd6
TomoyaShibata <wind.of.hometown@gmail.com>
2017-10-06 03:11:01 +0000

    :tada: いろいろ良くした
    雑

---
+++ Mastoon/Controls/BeautifulImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace Mastoon.Controls
+{
+    public class BeautifulImage : System.Windows.Controls.Image
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            this.VisualBitmapScalingMode = BitmapScalingMode.Fant;
+            base.OnRender(dc);
+        }
+    }
+}
---
+++ Mastoon/Controls/TimelineControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Mastoon.Controls
+{
+    /// <summary>
+    /// TimelineControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TimelineControl
+    {
+        public object ItemsSource
+        {
+            get => (object) this.GetValue(ItemsSourceProperty);
+            set => this.SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(object), typeof(TimelineControl),
+                new PropertyMetadata(null));
+
+        public object SelectedItem
+        {
+            get => (object) this.GetValue(SelectedItemProperty);
+            set => this.SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(TimelineControl),
+                new PropertyMetadata());
+
+        public TimelineControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
